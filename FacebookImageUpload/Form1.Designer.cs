d666eaf55ea832291bf9b89f5ff4abc5f7c365c8
niken0793 <tieudaodu_gaanmay@yahoo.com>
2015-10-14 21:56:08 +0700

    FacebookStegano

---
+++ FacebookImageUpload/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace FacebookImageUpload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openfile = new System.Windows.Forms.Button();
+            this.uploadImage = new System.Windows.Forms.Button();
+            this.imagePath = new System.Windows.Forms.TextBox();
+            this.downloadImage = new System.Windows.Forms.Button();
+            this.createAlbum = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openfile
+            // 
+            this.openfile.Location = new System.Drawing.Point(177, 26);
+            this.openfile.Name = "openfile";
+            this.openfile.Size = new System.Drawing.Size(27, 23);
+            this.openfile.TabIndex = 0;
+            this.openfile.Text = "...";
+            this.openfile.UseVisualStyleBackColor = true;
+            this.openfile.Click += new System.EventHandler(this.openfile_Click);
+            // 
+            // uploadImage
+            // 
+            this.uploadImage.Location = new System.Drawing.Point(129, 38);
+            this.uploadImage.Name = "uploadImage";
+            this.uploadImage.Size = new System.Drawing.Size(75, 23);
+            this.uploadImage.TabIndex = 2;
+            this.uploadImage.Text = "Upload";
+            this.uploadImage.UseVisualStyleBackColor = true;
+            this.uploadImage.Click += new System.EventHandler(this.uploadImage_Click);
+            // 
+            // imagePath
+            // 
+            this.imagePath.Location = new System.Drawing.Point(23, 40);
+            this.imagePath.Name = "imagePath";
+            this.imagePath.Size = new System.Drawing.Size(162, 20);
+            this.imagePath.TabIndex = 3;
+            // 
+            // downloadImage
+            // 
+            this.downloadImage.Location = new System.Drawing.Point(80, 218);
+            this.downloadImage.Name = "downloadImage";
+            this.downloadImage.Size = new System.Drawing.Size(75, 23);
+            this.downloadImage.TabIndex = 4;
+            this.downloadImage.Text = "Download";
+            this.downloadImage.UseVisualStyleBackColor = true;
+            this.downloadImage.Click += new System.EventHandler(this.downloadImage_Click);
+            // 
+            // createAlbum
+            // 
+            this.createAlbum.Location = new System.Drawing.Point(10, 37);
+            this.createAlbum.Name = "createAlbum";
+            this.createAlbum.Size = new System.Drawing.Size(92, 23);
+            this.createAlbum.TabIndex = 5;
+            this.createAlbum.Text = "Create Album";
+            this.createAlbum.UseVisualStyleBackColor = true;
+            this.createAlbum.Click += new System.EventHandler(this.createAlbum_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.openfile);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(228, 74);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Image Browse";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.createAlbum);
+            this.groupBox2.Controls.Add(this.uploadImage);
+            this.groupBox2.Location = new System.Drawing.Point(13, 102);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(228, 88);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Facebook Image Upload";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 372);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.downloadImage);
+            this.Controls.Add(this.imagePath);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openfile;
+        private System.Windows.Forms.Button uploadImage;
+        private System.Windows.Forms.TextBox imagePath;
+        private System.Windows.Forms.Button downloadImage;
+        private System.Windows.Forms.Button createAlbum;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ FacebookImageUpload/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Facebook;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Net;
+using System.Drawing.Imaging;
+namespace FacebookImageUpload
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string _imageId = "";
+        string _accessToken = "CAAVUMKQz7ZB0BAJG21S5gyhrBOpK7qvIZCViGPfbwMYckHWhXy8nPYcI5ZBxQOMZCjz5ieT9IydD6hDiE7sLEn6taU3K7ztbmZCgcHohnLqQw3vZAJPdXs5LjefrOEy4fIxQSPWXMe57n1TBCtmqUiWHdr4JSyq5ujBtQXWYlg5pdZBpQJncIvfU2rvPIkslQZCaZAs9ZAf8CRpQZDZD";
+        int image_source_height, image_source_width;
+        string _fileName = "", _fileNamewithoutExtension = "";
+        int temp = 0;
+        long _upFileSize, _downFileSize;
+        string _directory = @"E:\Tai lieu UIT\Khoa luan\Test\";
+        string _albumid = "";
+        private void openfile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    imagePath.Text = open.FileName;
+                    _fileName = Path.GetFileName(imagePath.Text);
+                    _fileNamewithoutExtension = Path.GetFileNameWithoutExtension(imagePath.Text);
+                    _upFileSize = new FileInfo(imagePath.Text).Length;
+                    var image_source = Image.FromFile(imagePath.Text);
+                    image_source_height = image_source.Height;
+                    image_source_width = image_source.Width;
+                }
+            }
+            catch (Exception)
+            {
+                throw new ApplicationException("Failed loading image");
+            }
+        }
+
+        private void createAlbum_Click(object sender, EventArgs e)
+        {
+            //create album
+            dynamic albumPost = new ExpandoObject();
+            albumPost.message = "Album desc";
+            albumPost.name = "Stegano";
+            var fb = new FacebookClient(_accessToken);
+            dynamic result = fb.Post("me/albums", albumPost);
+            _albumid = result.id;
+
+        }
+        private void uploadImage_Click(object sender, EventArgs e)
+        {
+            //upload photo
+            var imgstream = File.OpenRead(imagePath.Text);
+
+            dynamic res = fb.Post(_albumid + "/photos", new
+            {
+                message = "Image description",
+                file = new FacebookMediaStream{
+                    ContentType = "image/jpeg",
+                    FileName = _fileName,
+                }.SetValue(imgstream)
+            });
+            _imageId = res.id;        
+        }
+
+        private void downloadImage_Click(object sender, EventArgs e)
+        {
+            temp++;
+            var fb = new FacebookClient(_accessToken);
+            dynamic res = fb.Get(_imageId + "?fields=images");
+            string json_string = Newtonsoft.Json.JsonConvert.SerializeObject(res);
+            var json = JObject.Parse(json_string);
+            int i =0;
+            string source_url = "";
+            while ((string)json["images"][i].ToString()!=null)
+            {
+                int image_height = Int32.Parse((string)json["images"][i]["height"]);
+                int image_width = Int32.Parse((string)json["images"][i]["width"]);
+                if (image_height == image_source_height && image_width == image_source_width)
+                {
+                    source_url = (string)json["images"][i]["source"];
+                    break;
+                }
+                i++;
+
+            }
+            using (WebClient webClient = new WebClient())
+            {
+                byte[] data = webClient.DownloadData(source_url);
+
+                using (MemoryStream mem = new MemoryStream(data))
+                {
+                    using (var yourImage = Image.FromStream(mem))
+                    {
+                        yourImage.Save(_directory + _fileNamewithoutExtension + "_" + temp + ".jpg", ImageFormat.Jpeg);
+                        _downFileSize = new FileInfo(_directory + _fileNamewithoutExtension + "_" + temp + ".jpg").Length;
+                    }
+                }
+
+            }
+            long ratio = _upFileSize / _downFileSize ;
+            MessageBox.Show(ratio.ToString());
+        }
+
+    }
+}
