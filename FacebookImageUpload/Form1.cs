a2ec1da8420bfca1f874f86c17d0bb7479d6bfc1
niken0793 <tieudaodu_gaanmay@yahoo.com>
2015-10-14 22:35:41 +0700

    thêm comment

--- FacebookImageUpload/Form1.cs
+++ FacebookImageUpload/Form1.cs
@@ -8,14 +8,13 @@ using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using Facebook;
+using Facebook; // PM
 using Newtonsoft.Json.Linq;
 using System.IO;
 using System.Net;
 using System.Drawing.Imaging;
 namespace FacebookImageUpload
 {
-    // test commit code
     public partial class Form1 : Form
     {
         public Form1()
@@ -23,29 +22,29 @@ namespace FacebookImageUpload
             InitializeComponent();
         }
 
-        string _imageId = "";
+        string _imageId = ""; // lấy id của image đã up để query đường dẫn và độ phân giải ảnh
         string _accessToken = "CAAVUMKQz7ZB0BAJG21S5gyhrBOpK7qvIZCViGPfbwMYckHWhXy8nPYcI5ZBxQOMZCjz5ieT9IydD6hDiE7sLEn6taU3K7ztbmZCgcHohnLqQw3vZAJPdXs5LjefrOEy4fIxQSPWXMe57n1TBCtmqUiWHdr4JSyq5ujBtQXWYlg5pdZBpQJncIvfU2rvPIkslQZCaZAs9ZAf8CRpQZDZD";
-        int image_source_height, image_source_width;
-        string _fileName = "", _fileNamewithoutExtension = "";
-        int temp = 0;
-        long _upFileSize, _downFileSize;
-        string _directory = @"E:\Tai lieu UIT\Khoa luan\Test\";
-        string _albumid = "";
+        int image_source_height, image_source_width; // lấy resolution của ảnh gốc
+        string _fileName = "", _fileNamewithoutExtension = ""; // lấy tên file của ảnh
+        int temp = 0; // biến để đặt tên file tải về
+        long _upFileSize, _downFileSize;  // lấy dung lượng up và down để chia ra tỉ lệ
+        string _directory = @"E:\Tai lieu UIT\Khoa luan\Test\";  // đường dẫn thư mục download
+        string _albumid = ""; // lấy album id để up ảnh
         private void openfile_Click(object sender, EventArgs e)
         {
             try
             {
-                OpenFileDialog open = new OpenFileDialog();
+                OpenFileDialog open = new OpenFileDialog(); 
                 open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
                 if (open.ShowDialog() == DialogResult.OK)
                 {
                     imagePath.Text = open.FileName;
-                    _fileName = Path.GetFileName(imagePath.Text);
-                    _fileNamewithoutExtension = Path.GetFileNameWithoutExtension(imagePath.Text);
-                    _upFileSize = new FileInfo(imagePath.Text).Length;
-                    var image_source = Image.FromFile(imagePath.Text);
-                    image_source_height = image_source.Height;
-                    image_source_width = image_source.Width;
+                    _fileName = Path.GetFileName(imagePath.Text); // lấy file name
+                    _fileNamewithoutExtension = Path.GetFileNameWithoutExtension(imagePath.Text); // lấy file name ko có phần mở rộng .jpg
+                    _upFileSize = new FileInfo(imagePath.Text).Length; // lấy file size
+                    var image_source = Image.FromFile(imagePath.Text); 
+                    image_source_height = image_source.Height; // lấy width
+                    image_source_width = image_source.Width;   // lấy height
                 }
             }
             catch (Exception)
@@ -57,23 +56,23 @@ namespace FacebookImageUpload
         private void createAlbum_Click(object sender, EventArgs e)
         {
             //create album
-            dynamic albumPost = new ExpandoObject();
-            albumPost.message = "Album desc";
+            dynamic albumPost = new ExpandoObject();  // tạo đối tượng
+            albumPost.message = "Album desc";  // truyền tham số 
             albumPost.name = "Stegano";
             var fb = new FacebookClient(_accessToken);
-            dynamic result = fb.Post("me/albums", albumPost);
-            _albumid = result.id;
+            dynamic result = fb.Post("me/albums", albumPost); //request facebook api dạng: /{album-id}?field=message,name
+            _albumid = result.id; // lấy album id
 
         }
         private void uploadImage_Click(object sender, EventArgs e)
         {
             //upload photo
             var imgstream = File.OpenRead(imagePath.Text);
-            var fb = new FacebookClient(_accessToken);
+
             dynamic res = fb.Post(_albumid + "/photos", new
             {
                 message = "Image description",
-                file = new FacebookMediaStream{
+                file = new FacebookMediaStream{  
                     ContentType = "image/jpeg",
                     FileName = _fileName,
                 }.SetValue(imgstream)
